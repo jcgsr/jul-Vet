c4ac3aa3d93f22400c65a6c2545b384637311c48
jcgsr <cabralguerra@gmail.com>
2022-07-22 22:08:51 -0300

    layout, footer, header components

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en" className="dark">
+        <Head />
+        <body className="dark:bg-slate-800 dark:text-white">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
